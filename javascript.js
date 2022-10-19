60e0a420d2df7f87d768cf4bf7c2de9b9ca0d3a8
Rony Ahmmed <30334499+Rony7s@users.noreply.github.com>
2022-10-19 20:40:12 +0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// this function for change color of h1 id demo
+
+
+function change (x){
+    if(x==1){
+        document.getElementById("demo").style.color = "green";
+    }if(x==2){
+        document.getElementById("demo").style.color = "red";
+    }
+}
